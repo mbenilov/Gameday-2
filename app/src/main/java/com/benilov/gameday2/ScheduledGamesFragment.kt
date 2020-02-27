65d50308a90dc4afa79feb529a08538476c32ca1
Mark Benilov <mbenilov@wayfair.com>
2020-02-27 14:50:32 -0500

    Add view model

--- app/src/main/java/com/benilov/gameday2/ScheduledGamesFragment.kt
+++ app/src/main/java/com/benilov/gameday2/ScheduledGamesFragment.kt
@@ -1,7 +1,10 @@
 package com.benilov.gameday2
 
 import android.app.Fragment
+import com.benilov.gameday2.models.GameViewModel
 
 class ScheduledGamesFragment : Fragment() {
+
+    private lateinit var viewModel: GameViewModel
 
 }
---
+++ app/src/main/java/com/benilov/gameday2/models/GameViewModel.kt
@@ -0,0 +1,6 @@
+package com.benilov.gameday2.models
+
+import android.arch.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+}
